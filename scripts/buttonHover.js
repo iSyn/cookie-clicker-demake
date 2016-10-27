b6e3c087ab95a1890220c6ddc1cfc0740cd5546e
Synclair Wang <synclairwang11@yahoo.com>
2016-10-27 00:02:00 -0400

    styled the buttons a bit

---
+++ scripts/buttonHover.js
@@ -0,0 +1,18 @@
+$(() => {
+  console.log('buttonHover.js is linked')
+
+  // const $upgradesButton = $('.upgrades')
+
+  // const changeColor = (button) => {
+  //   $(button.target).css('color', 'red')
+  // }
+
+  // $upgradesButton.mouseover(() => {
+  //   changeColor()
+  // })
+
+  // $upgradesButton.mouseout(() => {
+  //   originalColor()
+  // })
+
+})
