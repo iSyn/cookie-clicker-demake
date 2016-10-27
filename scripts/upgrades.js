24da1a254d7f7cc586c84559b08adad45acb2819
Synclair Wang <synclairwang11@yahoo.com>
2016-10-27 00:57:17 -0400

    cookie counter working

--- scripts/upgrades.js
+++ scripts/upgrades.js
@@ -1,4 +1,18 @@
 $(() => {
   console.log('upgrades.js is linked')
 
+  let currentAmount = 0
+
+  const $cookie = $('.cookie')
+
+  const updateCookies = () => {
+    $('.current-amount').text(currentAmount)
+  }
+
+  $cookie.click(() => {
+    currentAmount += 1
+    updateCookies()
+  })
+
+
 })
