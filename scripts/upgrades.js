6a45791306c2f1750132257a4c25760bea606cee
Synclair Wang <synclairwang11@yahoo.com>
2016-10-27 01:17:10 -0400

    hid names of upgrades

--- scripts/upgrades.js
+++ scripts/upgrades.js
@@ -2,8 +2,35 @@ $(() => {
   console.log('upgrades.js is linked')
 
   let currentAmount = 0
-
+  let cookiesPerClick = 1
   const $cookie = $('.cookie')
+
+  let upgradeOneLocked = true;
+  let upgradeTwoLocked = true;
+  let upgradeThreeLocked = true;
+  let upgradeFourLocked = true;
+  let upgradeFiveLocked = true;
+  let upgradeSixLocked = true;
+
+  const updateUpgradeButtons = () => {
+    console.log('updateUpgradeButtons firing')
+    if (upgradeOneLocked === true) {
+      if (currentAmount >= 10) {
+        upgradeOneLocked = false
+        $('.upgrade1-name').text('Cursor')
+      }
+    } else if (upgradeTwoLocked === true) {
+      if (currentAmount >= 100) {
+        upgradeTwoLocked = false
+        $('.upgrade2-name').text('Grandma')
+      }
+    } else if (upgradeThreeLocked === true) {
+      if (currentAmount >= 1000) {
+        upgradeThreeLocked = false
+        $('.upgrade3-name').text('Farm')
+      }
+    }
+  }
 
   const updateCookies = () => {
     $('.current-amount').text(currentAmount)
@@ -12,6 +39,7 @@ $(() => {
   $cookie.click(() => {
     currentAmount += 1
     updateCookies()
+    updateUpgradeButtons()
   })
 
 
